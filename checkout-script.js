8527d61221b5a6afd9dd9447150349981c4b8f58
Qwixo <101059562+Qwixo@users.noreply.github.com>
2025-08-03 02:43:47 +0200

    Fix checkout-script.js ES module error

--- checkout-script.js
+++ checkout-script.js
@@ -1,3 +1,2 @@
 // Checkout functionality now handled by React app
 // Static checkout page redirects to React checkout page
-console.log('Static checkout page - redirects to React app for payment processing');
